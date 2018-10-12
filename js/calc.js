180cd16c8ce88b586391cfad277553fbf6fb5dce
Sana Rafaqat <sanarafaqat041@gmail.com>
2018-10-12 10:36:17 +0100

    Changed the code to make it more compact[C

--- js/calc.js
+++ js/calc.js
@@ -21,48 +21,57 @@
 // }
 
 document.addEventListener("DOMContentLoaded", function () {
-  var buttonNum = document.getElementsByTagName('button');
-  buttonNum[0].addEventListener('click', function (event) {
-    var displayNum = event.target.id;
-    document.getElementById('screen').innerHTML = displayNum;
-       console.log(displayNum);
+  var click = document.getElementsByTagName("button");
+  for (var i = 0; i < click.length; i++) {
+    click[i].addEventListener("click", function(event){
+      document.getElementById('screen').innerHTML=this.value;
+    })
+  }
 });
-  var clicks = document.querySelectorAll(".buttonNum");
-  for (var i = 0; i < clicks.length; i++) {
-    clicks[i].addEventListener('click', function (event) {
-       document.getElementById('screen').innerHTML=this.value;
-       console.log(this.value);
-  });
-}
-var clickOp = document.querySelectorAll(".operator");
-for (var i = 0; i < clickOp.length; i++) {
-  clickOp[i].addEventListener('click', function (event) {
-     document.getElementById('screen').innerHTML=this.value;
-     console.log(this.value);
-});
-}
-var clickClear = document.querySelectorAll(".buttonClear");
-for (var i = 0; i < clickClear.length; i++) {
-  clickClear[i].addEventListener('click', function (event) {
-     document.getElementById('screen').innerHTML=this.value;
-     console.log(this.value);
-});
-}
-var clickEquals = document.querySelectorAll(".equals");
-for (var i = 0; i < clickEquals.length; i++) {
-  clickEquals[i].addEventListener('click', function (event) {
-     document.getElementById('screen').innerHTML=this.value;
-     console.log(this.value);
-});
-}
-var clickPlus = document.querySelectorAll(".operatorPlus");
-for (var i = 0; i < clickPlus.length; i++) {
-  clickPlus[i].addEventListener('click', function (event) {
-     document.getElementById('screen').innerHTML=this.value;
-     console.log(this.value);
-});
-}
-});
+
+
+
+//   click[0].addEventListener('click', function (event) {
+//     var displayNum = event.target.id;
+//     document.getElementById('screen').innerHTML = displayNum;
+//        console.log(displayNum);
+// });
+//   var clicks = document.querySelectorAll(".buttonNum");
+//   for (var i = 0; i < clicks.length; i++) {
+//     clicks[i].addEventListener('click', function (event) {
+//        document.getElementById('screen').innerHTML=this.value;
+//        console.log(this.value);
+//   });
+// }
+// var clickOp = document.querySelectorAll(".operator");
+// for (var i = 0; i < clickOp.length; i++) {
+//   clickOp[i].addEventListener('click', function (event) {
+//      document.getElementById('screen').innerHTML=this.value;
+//      console.log(this.value);
+// });
+// }
+// var clickClear = document.querySelectorAll(".buttonClear");
+// for (var i = 0; i < clickClear.length; i++) {
+//   clickClear[i].addEventListener('click', function (event) {
+//      document.getElementById('screen').innerHTML=this.value;
+//      console.log(this.value);
+// });
+// }
+// var clickEquals = document.querySelectorAll(".equals");
+// for (var i = 0; i < clickEquals.length; i++) {
+//   clickEquals[i].addEventListener('click', function (event) {
+//      document.getElementById('screen').innerHTML=this.value;
+//      console.log(this.value);
+// });
+// }
+// var clickPlus = document.querySelectorAll(".operatorPlus");
+// for (var i = 0; i < clickPlus.length; i++) {
+//   clickPlus[i].addEventListener('click', function (event) {
+//      document.getElementById('screen').innerHTML=this.value;
+//      console.log(this.value);
+// });
+// }
+// });
 // var myButton = document.getElementsByTagName("Button");
 // myButton[]. addEventListener("click", function (event) {
 //     console.log("Button Clicked" + this);
